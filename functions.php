b2025e70d19bacb1297eded894ac80d463af2da2
botoole <botoole518@gmail.com>
2018-08-15 17:02:38 -0400

    lib layout

--- functions.php
+++ functions.php
@@ -1,14 +1,19 @@
 <?php
 /**
- * Genesis Child Starter Theme (Gcst)
+ * 
+ * Functions.php = Launching point to get us up and running
  *
  * @package     BrianOToole\GCST
- * @since       1.0.2
+ * @since       1.0.0
  * @author      @brianotoole
  * @link        https://brianzotoole.com
  * @license     GNU General Public License 2.0+
  */
 
 namespace BrianOToole\Gcst;
+
+/* Start up */
 include_once( 'lib/init.php' );
+
+/* Setup */
 include_once( 'lib/functions/autoload.php' );
---
+++ lib/init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Genesis Child Starter Theme (Gcst)
+ *
+ * @package     BrianOToole\GCST
+ * @since       1.0.0
+ * @author      @brianotoole
+ * @link        https://brianzotoole.com
+ * @license     GNU General Public License 2.0+
+ */
+
+namespace BrianOToole\Gcst;
