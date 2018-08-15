c66f5f070769aacbf58597523f5b0363d9e0da28
botoole <botoole518@gmail.com>
2018-08-15 17:19:07 -0400

    add default functions

---
+++ config/theme.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Theme.php = theme specifics
+ *
+ * @package     BrianOToole\GCST
+ * @since       1.0.0
+ * @author      @brianotoole
+ * @link        https://brianzotoole.com
+ * @license     GNU General Public License 2.0+
+ */
+
+namespace BrianOToole\Gcst;
